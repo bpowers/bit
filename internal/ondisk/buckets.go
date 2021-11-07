d2c319cb264946224b5324b076f93618a60b775a
Bobby Powers <bobbypowers@gmail.com>
2021-11-06 22:36:23 -0700

    ondisk: cleanup buckets a bit

--- internal/ondisk/buckets.go
+++ internal/ondisk/buckets.go
@@ -14,8 +14,8 @@ import (
 
 type BucketSlice struct {
 	f         *os.File
-	len       int // length in number of elements
-	bucketCap int // offset in bytes of the start of this slice
+	nBuckets  int64 // length in number of elements
+	bucketCap int64 // offset in bytes of the start of this slice
 	bucketBuf []byte
 	valuesBuf []uint32
 }
@@ -24,20 +24,20 @@ const (
 	initialBucketSize = 4
 )
 
-func bucketLen(bucketCap int) int {
+func bucketLen(bucketCap int64) int64 {
 	// uint32 `n`
 	// uint32 `bucketLen`
 	// uint32 * bucketSize items
 	return 4 + 4 + 4*bucketCap
 }
 
-func NewBucketSlice(f *os.File, len int64) (*BucketSlice, error) {
+func NewBucketSlice(f *os.File, nBuckets int64) (*BucketSlice, error) {
 	if err := f.Truncate(0); err != nil {
 		return nil, fmt.Errorf("f.Truncate: %e", err)
 	}
 	b := &BucketSlice{
-		f:   f,
-		len: int(len),
+		f:        f,
+		nBuckets: nBuckets,
 	}
 	if err := b.setBucketCapacity(initialBucketSize); err != nil {
 		return nil, err
@@ -45,16 +45,16 @@ func NewBucketSlice(f *os.File, len int64) (*BucketSlice, error) {
 	return b, nil
 }
 
-func readBucket(f *os.File, b []byte, off, bucketCap int) error {
+func readBucket(f *os.File, b []byte, off, bucketCap int64) error {
 	bucketSize := bucketLen(bucketCap)
-	if len(b) < bucketSize {
+	if int64(len(b)) < bucketSize {
 		return fmt.Errorf("readBucket(f, b, %d, %d): elemLen(b) (%d) too short for bucketCap", off, bucketCap, len(b))
 	}
 	_, err := f.ReadAt(b, int64(off*bucketSize))
 	return err
 }
 
-func (s *BucketSlice) setBucketCapacity(newBucketCap int) error {
+func (s *BucketSlice) setBucketCapacity(newBucketCap int64) error {
 	oldBucketCap := s.bucketCap
 	if oldBucketCap > newBucketCap {
 		return fmt.Errorf("newBucketCap %d needs to be greater than old cap %d", newBucketCap, oldBucketCap)
@@ -70,7 +70,7 @@ func (s *BucketSlice) setBucketCapacity(newBucketCap int) error {
 
 	s.bucketCap = newBucketCap
 
-	if err := s.f.Truncate(int64(s.len * newBucketSize)); err != nil {
+	if err := s.f.Truncate(s.nBuckets * newBucketSize); err != nil {
 		return fmt.Errorf("f.Truncate: %e", err)
 	}
 
@@ -83,12 +83,12 @@ func (s *BucketSlice) setBucketCapacity(newBucketCap int) error {
 
 	// iterate in reverse order so that we are always moving a bucket into a new (or no longer
 	// needed) part of the file
-	for i := s.len - 1; i >= 0; i-- {
+	for i := s.nBuckets - 1; i >= 0; i-- {
 		zero.Bytes(bucketBuf)
 		if err := readBucket(s.f, bucketBuf[0:oldBucketSize], i, oldBucketCap); err != nil {
 			return fmt.Errorf("readBucket: %e", err)
 		}
-		_, err := s.f.WriteAt(bucketBuf, int64(i*newBucketSize))
+		_, err := s.f.WriteAt(bucketBuf, i*newBucketSize)
 		if err != nil {
 			return fmt.Errorf("writeBucket: %e", err)
 		}
@@ -98,26 +98,26 @@ func (s *BucketSlice) setBucketCapacity(newBucketCap int) error {
 }
 
 func (s *BucketSlice) AddToBucket(off int64, n int32) error {
-	if off < 0 || off >= int64(s.len) {
+	if off < 0 || off >= s.nBuckets {
 		return fmt.Errorf("byteOff %d out of range", off)
 	}
 	bucketBuf := s.bucketBuf[0:bucketLen(s.bucketCap)]
 	zero.Bytes(bucketBuf)
-	if err := readBucket(s.f, bucketBuf, int(off), s.bucketCap); err != nil {
+	if err := readBucket(s.f, bucketBuf, off, s.bucketCap); err != nil {
 		return fmt.Errorf("readBucket: %e", err)
 	}
 	bucketOff := binary.LittleEndian.Uint32(bucketBuf[0:4])
 	if bucketOff == 0 && off != 0 {
 		binary.LittleEndian.PutUint32(bucketBuf[0:4], uint32(off))
 	}
-	valuesLen := int(binary.LittleEndian.Uint32(bucketBuf[4:8]))
+	valuesLen := int64(binary.LittleEndian.Uint32(bucketBuf[4:8]))
 	if valuesLen >= s.bucketCap {
 		if err := s.setBucketCapacity(s.bucketCap * 2); err != nil {
 			return err
 		}
 		bucketBuf = s.bucketBuf[0:bucketLen(s.bucketCap)]
 		zero.Bytes(bucketBuf)
-		if err := readBucket(s.f, bucketBuf, int(off), s.bucketCap); err != nil {
+		if err := readBucket(s.f, bucketBuf, off, s.bucketCap); err != nil {
 			return fmt.Errorf("readBucket: %e", err)
 		}
 	}
@@ -134,18 +134,18 @@ type Bucket struct {
 
 func (s *BucketSlice) Bucket(off int) (Bucket, error) {
 	bucketSize := bucketLen(s.bucketCap)
-	if len(s.bucketBuf) < bucketSize {
+	if len(s.bucketBuf) < int(bucketSize) {
 		s.bucketBuf = make([]byte, bucketSize)
 	}
 	buf := s.bucketBuf[:bucketSize]
 	zero.Bytes(buf)
-	if err := readBucket(s.f, buf, off, s.bucketCap); err != nil {
+	if err := readBucket(s.f, buf, int64(off), s.bucketCap); err != nil {
 		return Bucket{}, err
 	}
 
 	n := int(binary.LittleEndian.Uint32(buf[0:4]))
-	valuesLen := int(binary.LittleEndian.Uint32(buf[4:8]))
-	if len(s.valuesBuf) < valuesLen {
+	valuesLen := int64(binary.LittleEndian.Uint32(buf[4:8]))
+	if int64(len(s.valuesBuf)) < valuesLen {
 		s.valuesBuf = make([]uint32, valuesLen)
 	}
 	if valuesLen >= s.bucketCap {
@@ -153,7 +153,7 @@ func (s *BucketSlice) Bucket(off int) (Bucket, error) {
 	}
 	values := s.valuesBuf[0:valuesLen]
 	zero.Uint32(values)
-	for i := 0; i < valuesLen; i++ {
+	for i := int64(0); i < valuesLen; i++ {
 		v := binary.LittleEndian.Uint32(buf[8+4*i : 8+4*i+4])
 		values[i] = v
 	}
@@ -165,12 +165,12 @@ func (s *BucketSlice) Bucket(off int) (Bucket, error) {
 }
 
 func (s *BucketSlice) Len() int {
-	return s.len
+	return int(s.nBuckets)
 }
 
 func (s *BucketSlice) Less(i, j int) bool {
 	l := bucketLen(s.bucketCap)
-	if len(s.bucketBuf) < l*2 {
+	if len(s.bucketBuf) < int(l*2) {
 		s.bucketBuf = make([]byte, l*2)
 	}
 	iBuf := s.bucketBuf[:l]
@@ -178,10 +178,10 @@ func (s *BucketSlice) Less(i, j int) bool {
 	zero.Bytes(iBuf)
 	zero.Bytes(jBuf)
 	// capacity of zero -- we only care about the headers
-	if err := readBucket(s.f, iBuf[:], i, s.bucketCap); err != nil {
+	if err := readBucket(s.f, iBuf[:], int64(i), s.bucketCap); err != nil {
 		panic(err)
 	}
-	if err := readBucket(s.f, jBuf[:], j, s.bucketCap); err != nil {
+	if err := readBucket(s.f, jBuf[:], int64(j), s.bucketCap); err != nil {
 		panic(err)
 	}
 	iLen := int(binary.LittleEndian.Uint32(iBuf[4:8]))
@@ -190,22 +190,22 @@ func (s *BucketSlice) Less(i, j int) bool {
 }
 func (s *BucketSlice) Swap(i, j int) {
 	l := bucketLen(s.bucketCap)
-	if len(s.bucketBuf) < l*2 {
+	if len(s.bucketBuf) < int(l*2) {
 		s.bucketBuf = make([]byte, l*2)
 	}
 	iBuf := s.bucketBuf[:l]
 	jBuf := s.bucketBuf[l:]
 
-	if err := readBucket(s.f, iBuf, i, s.bucketCap); err != nil {
+	if err := readBucket(s.f, iBuf, int64(i), s.bucketCap); err != nil {
 		panic(err)
 	}
-	if err := readBucket(s.f, jBuf, j, s.bucketCap); err != nil {
+	if err := readBucket(s.f, jBuf, int64(j), s.bucketCap); err != nil {
 		panic(err)
 	}
-	if _, err := s.f.WriteAt(jBuf, int64(i*l)); err != nil {
+	if _, err := s.f.WriteAt(jBuf, int64(i)*l); err != nil {
 		panic(err)
 	}
-	if _, err := s.f.WriteAt(iBuf, int64(j*l)); err != nil {
+	if _, err := s.f.WriteAt(iBuf, int64(j)*l); err != nil {
 		panic(err)
 	}
 }
