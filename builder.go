9980df2a35f49961f0d50f46a5a3b6a363aad678
Bobby Powers <bobbypowers@gmail.com>
2023-11-27 15:41:10 -0800

    table: move Builder into its own file

---
+++ builder.go
@@ -0,0 +1,122 @@
+// Copyright 2021 The bit Authors. All rights reserved.
+// Use of this source code is governed by the MIT License
+// that can be found in the LICENSE file.
+
+package bit
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/bpowers/bit/internal/datafile"
+	"github.com/bpowers/bit/internal/index"
+)
+
+var (
+	errKeyTooBig = errors.New("we only support keys < 256 bytes in length")
+)
+
+// Builder is used to construct a big immutable table from key/value pairs.
+type Builder struct {
+	resultPath string
+	dataFile   *os.File
+	dioWriter  *datafile.Writer
+}
+
+// NewBuilder creates a Builder that can be used to construct a Table.  Building should happen
+// once, and the table
+func NewBuilder(dataFilePath string) (*Builder, error) {
+	// we want to write to a new file and do an atomic rename when we're done on disk
+	dataFilePath, err := filepath.Abs(dataFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("filepath.Abs: %w", err)
+	}
+	dir := filepath.Dir(dataFilePath)
+	dataFile, err := os.CreateTemp(dir, "bit-builder.*.data")
+	if err != nil {
+		return nil, fmt.Errorf("CreateTemp failed (may need permissions for dir %q containing dataFile): %w", dir, err)
+	}
+	w, err := datafile.NewWriter(dataFile)
+	if err != nil {
+		return nil, fmt.Errorf("datafile.NewWriter: %w", err)
+	}
+	return &Builder{
+		resultPath: dataFilePath,
+		dataFile:   dataFile,
+		dioWriter:  w,
+	}, nil
+}
+
+// Put adds a key/value pair to the table.  Duplicate keys result in an error at Finalize time.
+func (b *Builder) Put(k, v []byte) error {
+	kLen := len(k)
+	if kLen > datafile.MaxKeyLen {
+		return errKeyTooBig
+	}
+	_, err := b.dioWriter.Write(k, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Builder) Finalize() (*Table, error) {
+	return b.finalize()
+}
+
+// Finalize flushes the table to disk and builds an index to efficiently randomly access entries.
+func (b *Builder) finalize() (*Table, error) {
+	if err := b.dioWriter.Finish(); err != nil {
+		return nil, fmt.Errorf("recordio.Close: %w", err)
+	}
+
+	if err := appendIndexFor(b.dataFile, b.dioWriter); err != nil {
+		return nil, fmt.Errorf("appendIndexFor: %w\n", err)
+	}
+
+	// make the file read-only
+	if err := os.Chmod(b.dataFile.Name(), 0444); err != nil {
+		return nil, fmt.Errorf("os.Chmod(0444): %w", err)
+	}
+	if err := os.Rename(b.dataFile.Name(), b.resultPath); err != nil {
+		return nil, fmt.Errorf("os.Rename: %w", err)
+	}
+	// make the file read-only
+	if err := os.Chmod(b.resultPath, 0444); err != nil {
+		return nil, fmt.Errorf("os.Chmod(0444): %w", err)
+	}
+	_ = b.dataFile.Close()
+	b.dataFile = nil
+	dataPath := b.resultPath
+
+	return New(dataPath)
+}
+
+func appendIndexFor(f *os.File, dioWriter *datafile.Writer) error {
+	dataPath := f.Name()
+	r, err := datafile.NewMMapReaderWithPath(dataPath)
+	if err != nil {
+		return fmt.Errorf("datafile.NewMMapReaderAtPath(%s): %w", dataPath, err)
+	}
+
+	it := r.Iter()
+	defer it.Close()
+	if built, err := index.Build(it); err != nil {
+		_ = f.Close()
+		_ = os.Remove(f.Name())
+		return fmt.Errorf("idx.Build: %w", err)
+	} else {
+		if err := dioWriter.SetIndexMetadata(built.Level0Len, built.Level1Len); err != nil {
+			return fmt.Errorf("dioWriter.SetIndexMetadata: %w", err)
+		}
+		if n, err := f.Write(built.Table); err != nil {
+			return fmt.Errorf("idx.Write: %w", err)
+		} else if n != len(built.Table) {
+			return fmt.Errorf("idx.Write: short write of %d (wanted %d)", n, len(built.Table))
+		}
+	}
+
+	return nil
+}
